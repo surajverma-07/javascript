14e58a96438a93098b13c98ffa4a8d33f41b13ee
SURAJ KUMAR VERMA <137256443+surajverma-07@users.noreply.github.com>
2024-06-19 19:57:44 +0530

    data types

--- 1variables.js
+++ 1variables.js
@@ -3,12 +3,12 @@ const accountId = 174757
 let accountEmail = "suraj@gmail.com"
 var accountPassword = "12345"
 accountCity = "Jaipur"
-
+let  accountState;
 // accountId = 2 not allowed
 accountEmail = "suraj1234@gmail.com"
 accountPassword = "54321"
 console.log(accountId);
-console.table([accountId,accountEmail,accountPassword,accountCity])
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
 /*
  Prefer not to use var because it tends to scope issue 
 */
---
+++ 2dataTypes.js
@@ -0,0 +1,20 @@
+"use strict";// treat all JS code as newer version 
+
+// alert("Jay shree ram") we are using nodeJs not browser so it will throw an error
+
+let name = "suraj"
+let age = 20
+let isLoggedIn = true
+
+// number -> 2^53
+//bigint
+//string
+//boolean
+//null -> standalone value
+//undefined -> not defined
+//symbol -> to find uniqueness 
+
+//Object
+console.log(typeof age);//number
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
